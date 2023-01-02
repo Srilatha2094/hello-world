8e83d3438277cb583a1486ff4b5e58e80ac73198
Srilatha2094 <117397567+Srilatha2094@users.noreply.github.com>
2023-01-02 14:16:38 -0800

    commit all changes

--- nav.js
+++ nav.js
@@ -1,11 +1,18 @@
-var menu_btn = document.querySelector("#menu-btn");
-var sidebar = document.querySelector("#sidebar");
-var container = document.querySelector(".my-container");
-menu_btn.addEventListener("click", () => {
-  sidebar.classList.toggle("active-nav");
-  container.classList.toggle("active-cont");
-  console.log("Srilatha4");
-  console.log("srilatha");
-  console.log("srilatha1");
-  console.log("srilatha5");
+jQuery(".nav-folderized h4").click(function () {
+  jQuery(this).parent(".nav").toggleClass("open");
+  jQuery("html, body").animate(
+    { scrollTop: jQuery(this).offset().top - 170 },
+    1500
+  );
 });
+// var menu_btn = document.querySelector("#menu-btn");
+// var sidebar = document.querySelector("#sidebar");
+// var container = document.querySelector(".my-container");
+// menu_btn.addEventListener("click", () => {
+//   sidebar.classList.toggle("active-nav");
+//   container.classList.toggle("active-cont");
+//   console.log("Srilatha4");
+//   console.log("srilatha");
+//   console.log("srilatha1");
+//   console.log("srilatha5");
+// });
