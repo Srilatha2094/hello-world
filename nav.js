4c03062be4244f627e7c944f9ad37d5f9f3a3314
Srilatha2094 <117397567+Srilatha2094@users.noreply.github.com>
2023-06-09 14:15:50 -0700

    removed

--- nav.js
+++ nav.js
@@ -4,7 +4,6 @@ var container = document.querySelector(".my-container");
 menu_btn.addEventListener("click", () => {
   sidebar.classList.toggle("active-nav");
   container.classList.toggle("active-cont");
-  console.log("Srilatha4");
   console.log("srilatha");
   console.log("srilatha1");
 });
