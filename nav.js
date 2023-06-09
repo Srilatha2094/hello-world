33cea00bbe9d3cc41bdcc576a3d5964677131d51
Srilatha2094 <117397567+Srilatha2094@users.noreply.github.com>
2023-06-09 16:44:51 -0700

    removed line

--- nav.js
+++ nav.js
@@ -5,5 +5,4 @@ menu_btn.addEventListener("click", () => {
   sidebar.classList.toggle("active-nav");
   container.classList.toggle("active-cont");
   console.log("srilatha");
-  console.log("srilatha1");
 });
